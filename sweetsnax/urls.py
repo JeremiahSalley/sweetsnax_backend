1b0974863ff4dc76982778d8ef5e2acdd8aeb3b8
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2020-12-15 17:04:08 -0500

    post

--- sweetsnax/urls.py
+++ sweetsnax/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.order_list, name='order_list'),
     path('orders/<str:pk>/', views.order_detail, name='order_detail'),
-    path('order/new', views.OrderList.as_view(), name='orderCreate'),
+    path('order/new', views.OrderLista.as_view(), name='orderCreate'),
     path('order/update/<str:pk>/', views.orderUpdate, name='orderUpdate'),
     path('order/delete/<str:pk>/', views.orderUpdate, name='orderDelete'),
 
