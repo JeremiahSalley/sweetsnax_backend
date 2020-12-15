75f56dea3ca5bd3bbedcc3ff6f4f19c923484489
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2020-12-15 12:15:13 -0500

    test

--- sweetsnax_django/settings.py
+++ sweetsnax_django/settings.py
@@ -72,18 +72,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sweetsnax_django.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'snax',
-        'USER': 'snaxuser',
-        'PASSWORD': 'sweet',
-        'HOST': 'localhost'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'snax',
+#         'USER': 'snaxuser',
+#         'PASSWORD': 'sweet',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
