843973aec9c3f50ab5389c050cad9a9399647071
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2020-12-15 15:31:57 -0500

    added cors

--- sweetsnax_django/settings.py
+++ sweetsnax_django/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sweetsnax',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+CORS_ALLOWED_ORIGINS = (
+    'localhost:3000'
+)
 
 ROOT_URLCONF = 'sweetsnax_django.urls'
 
